1d8f1febef96eeed8aea11669f7ce8511483a34d
Juan esteban molano <juanestebanmolanog@gmail.com>
2024-10-23 17:37:07 -0500

    subiendo crud log juan esteban

---
+++ VOTACIONES-APP/Logic/Usuarios_afkLog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Logic
+{
+    public class Usuarios_afkLog
+    {
+        Usuarios_afkLog objAFK = new Usuarios_afkLog();
+
+        public DataSet showUsuariosAfk()
+        {
+            return objAFK.showUsuariosAfk();
+        }
+        public bool saveUsuarioAfk(string correo, string contrasena)
+        {
+            return objAFK.saveUsuarioAfk(correo, contrasena);
+        }
+        public bool updateUsuarioAfk(int id, string correo, string contrasena)
+        {
+            return objAFK.updateUsuarioAfk(id,correo, contrasena);
+        }
+        public bool deleteUsuarioAfk(int id)
+        {
+            return objAFK.deleteUsuarioAfk(id);
+        }
+
+    }
+}
